11e69821d1b490d35aa566604969e7db8a458e7d
arbelos <senad.ibraimoski@sbgenomics.com>
2015-03-31 11:04:01 +0200

    assertEquals, assertNotEquals depracted in py3

--- pymailinator/tests/test_wrapper.py
+++ pymailinator/tests/test_wrapper.py
@@ -52,7 +52,7 @@ class TestWrapper(unittest.TestCase):
         inbox = wrapper.Inbox('123')
         inbox.get()
         self.assertEqual(inbox.count(), 0)
-        self.assertEquals(inbox.messages, [])
+        self.assertEqual(inbox.messages, [])
 
     def test_bad_token(self):
         wrapper.urlopen = get_bad_api_token
@@ -83,7 +83,7 @@ class TestWrapper(unittest.TestCase):
         wrapper.urlopen = get_message
         message = inbox.messages[0]
         message.get_message()
-        self.assertNotEquals(message.body, '')
+        self.assertNotEqual(message.body, '')
 
     def test_missing_message(self):
         wrapper.urlopen = get_mailbox
@@ -97,14 +97,14 @@ class TestWrapper(unittest.TestCase):
         wrapper.urlopen = get_mailbox
         inbox = wrapper.Inbox('123')
         inbox.get()
-        self.assertEquals(type(inbox.view_subjects()), list)
+        self.assertEqual(type(inbox.view_subjects()), list)
         self.assertGreater(len(inbox.view_subjects()), 0)
 
     def test_view_message_ids(self):
         wrapper.urlopen = get_mailbox
         inbox = wrapper.Inbox('123')
         inbox.get()
-        self.assertEquals(type(inbox.view_message_ids()), list)
+        self.assertEqual(type(inbox.view_message_ids()), list)
         self.assertGreater(len(inbox.view_message_ids()), 0)
 
     def test_get_message_by_subject(self):
@@ -112,23 +112,23 @@ class TestWrapper(unittest.TestCase):
         inbox = wrapper.Inbox('123')
         inbox.get()
         message = inbox.get_message_by_subject('Want to cheat? ')
-        self.assertEquals(message.subject, 'Want to cheat? ')
+        self.assertEqual(message.subject, 'Want to cheat? ')
 
     def test_get_message_by_id(self):
         wrapper.urlopen = get_mailbox
         inbox = wrapper.Inbox('123')
         inbox.get()
         message = inbox.get_message_by_id('1418740612-3134545-m8r-rmtci4')
-        self.assertEquals(message.id, '1418740612-3134545-m8r-rmtci4')
+        self.assertEqual(message.id, '1418740612-3134545-m8r-rmtci4')
 
     def test_filter_inbox(self):
         wrapper.urlopen = get_mailbox
         inbox = wrapper.Inbox('123')
         inbox.get()
         filtered_me = inbox.filter('to', 'me')
-        self.assertEquals(len(filtered_me), 0)
+        self.assertEqual(len(filtered_me), 0)
         filtered = inbox.filter('to', 'm8r-rmtci4@mailinator.com')
-        self.assertEquals(len(filtered), 2)
+        self.assertEqual(len(filtered), 2)
 
     def test_other_mailbox(self):
         wrapper.urlopen = get_mailbox
